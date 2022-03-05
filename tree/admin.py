9295cae93025afebbc11e6afca81937e54f3db35
Valery Efremov <valeriy131100@gmail.com>
2022-03-05 13:32:03 +0300

    Create Employee model

--- tree/admin.py
+++ tree/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+
+from .models import Employee
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
--- tree/models.py
+++ tree/models.py
@@ -1,1 +1,22 @@
 from django.db import models
+
+
+class Employee(models.Model):
+    full_name = models.CharField(
+        verbose_name='ФИО',
+        max_length=200
+    )
+    position = models.CharField(
+        verbose_name='Должность',
+        max_length=200
+    )
+    employment_date = models.DateField(
+        verbose_name='Дата трудоустройства'
+    )
+    boss = models.ForeignKey(
+        'Employee',
+        verbose_name='Начальник',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
