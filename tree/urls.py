b34f9bbe94108a303cde1487224541717f5f5506
Valery Efremov <valeriy131100@gmail.com>
2022-03-05 18:01:40 +0300

    Load employees without boss

--- tree/urls.py
+++ tree/urls.py
@@ -4,7 +4,7 @@ from .views import index, get_subordinates
 
 urlpatterns = [
     path('', index, name='index'),
-    path('api/subordinates/', get_subordinates, name='subordinates'),
+    path('api/subordinates/', get_subordinates, name='root_subordinates'),
     path(
         'api/subordinates/<int:start_from>/',
         get_subordinates,
